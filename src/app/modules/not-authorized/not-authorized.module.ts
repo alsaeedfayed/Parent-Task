ab1212fbebcc472c1cfed8ed553acb808673d66f
alsaeedfayed <alsaeedfayed@gmail.com>
2024-05-23 09:44:16 -0800

    project-structure

---
+++ src/app/modules/not-authorized/not-authorized.module.ts
@@ -0,0 +1,14 @@
+import { NgModule } from '@angular/core';
+import { CommonModule } from '@angular/common';
+
+import { NotAuthorizedRoutingModule } from './not-authorized-routing.module';
+
+
+@NgModule({
+  declarations: [],
+  imports: [
+    CommonModule,
+    NotAuthorizedRoutingModule
+  ]
+})
+export class NotAuthorizedModule { }
---
+++ src/app/modules/user-details/user-details.module.ts
@@ -0,0 +1,14 @@
+import { NgModule } from '@angular/core';
+import { CommonModule } from '@angular/common';
+
+import { UserDetailsRoutingModule } from './user-details-routing.module';
+
+
+@NgModule({
+  declarations: [],
+  imports: [
+    CommonModule,
+    UserDetailsRoutingModule
+  ]
+})
+export class UserDetailsModule { }
