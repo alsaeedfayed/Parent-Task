b1bc2ae46339def27ab0adcd153991bb7b225a17
alsaeedfayed <alsaeedfayed@gmail.com>
2024-05-23 11:42:54 -0800

    building generic class  http-methods handler and intializing the server url in the environment

--- src/app/core/services/http-handler/http-handler.service.ts
+++ src/app/core/services/http-handler/http-handler.service.ts
@@ -1,9 +1,92 @@
+import { HttpClient, HttpErrorResponse, HttpHeaders, HttpParams } from '@angular/common/http';
 import { Injectable } from '@angular/core';
+import { catchError, Observable, throwError } from 'rxjs';
+import { environment } from '../../../../environments/environment';
 
 @Injectable({
   providedIn: 'root'
 })
-export class HttpHandlerService {
+export class HttpService {
 
-  constructor() { }
+  constructor(private http: HttpClient) { }
+
+  get<T>(url: string, params?: any, headers?: HttpHeaders): Observable<T | T[]> {
+    const options = {
+      params: new HttpParams({ fromObject: params }),
+      headers: headers ? headers : new HttpHeaders()
+    }
+    return this.http.get<T | T[]>(this.getURL(url), options).pipe(
+      catchError(this.handleError)
+    );
+  }
+
+  post<T>(url: string, body: T, params?: any, headers?: HttpHeaders): Observable<T> {
+    const options = {
+      params: new HttpParams({ fromObject: params }),
+      headers: headers ? headers : new HttpHeaders()
+    }
+    return this.http.post<T>(this.getURL(url), body, options).pipe(
+      catchError(this.handleError)
+    );
+  }
+
+
+  put<T>(url: string, body: T, params?: any, headers?: HttpHeaders): Observable<T> {
+    const options = {
+      params: new HttpParams({ fromObject: params }),
+      headers: headers ? headers : new HttpHeaders()
+    }
+    return this.http.post<T>(this.getURL(url), body, options).pipe(
+      catchError(this.handleError)
+    );
+  }
+
+
+  patch<T>(url: string, body: T, params?: any, headers?: HttpHeaders): Observable<T> {
+    const options = {
+      params: new HttpParams({ fromObject: params }),
+      headers: headers ? headers : new HttpHeaders()
+    }
+    return this.http.post<T>(this.getURL(url), body, options).pipe(
+      catchError(this.handleError)
+    );
+  }
+
+
+
+  delete<T>(url: string, params?: any, headers?: HttpHeaders): Observable<T> {
+    const options = {
+      params: new HttpParams({ fromObject: params }),
+      headers: headers ? headers : new HttpHeaders()
+    }
+    return this.http.get<T>(this.getURL(url), options).pipe(
+      catchError(this.handleError)
+    );
+  }
+
+
+  //Get the url
+  private getURL(path: string): string {
+    //pass the entire path from the param or use the base in the env and put the rest of the path.
+    if (path && (path.includes('http://') || path.includes('https://'))) return path
+    return environment.serverUrl + path
+  }
+
+
+  //Handle errors funciton
+  private handleError(error : HttpErrorResponse){
+    let errorMsg = "An error occurred: ";
+    if (error.error instanceof ErrorEvent) {
+      // A client-side or network error occurred. Handle it accordingly.
+      errorMsg = 'An error occurred:', error.error.message;
+
+    } else {
+      // The backend returned an unsuccessful response code.
+        errorMsg = `Backend returned code ${error.message}`
+    }
+    // return an observable with a user-facing error message
+    return throwError( errorMsg)
+  }
 }
+
+
